33f40e9c63f7dff664060c0c9decd87f27205edc
Talia Aziz Khan <talyaaziz@gmail.com>
2021-12-03 04:09:55 +0000

    updated

--- index2.js
+++ index2.js
@@ -1,19 +1,12 @@
 // index2.js
 
-const { fetchMyIP } = require('./iss_promised');
-
-  fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+const { nextISSTimesForMyLocation, printPassTimes } = require('./iss_promised');
 
 
-const { nextISSTimesForMyLocation } = require('./iss_promised');
-
-// see index.js for printPassTimes 
-// copy it from there, or better yet, moduralize and require it in both files
-
-nextISSTimesForMyLocation()
+  nextISSTimesForMyLocation()
   .then((passTimes) => {
     printPassTimes(passTimes);
   })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
--- iss_promised.js
+++ iss_promised.js
@@ -1,14 +1,21 @@
 // iss_promised.js
+
 const request = require('request-promise-native');
+
+// ----------------------------------------------------------------------------------------------
 
 const fetchMyIP = () => {
   return request('https://api.ipify.org?format=json');
 };
 
+//-----------------------------------------------------------------------------------------------
+
 const fetchCoordsByIP = function(body) {
   const ip = JSON.parse(body).ip;
   return request(`https://freegeoip.app/json/${ip}`);
 };
+
+//------------------------------------------------------------------------------------------------
 
 
 const fetchISSFlyOverTimes = function(body) {
@@ -17,7 +24,7 @@ const fetchISSFlyOverTimes = function(body) {
   return request(url);
 };
 
-
+//--------------------------------------------------------------------------------------------------
 
 const nextISSTimesForMyLocation = function() {
   return fetchMyIP()
@@ -29,9 +36,18 @@ const nextISSTimesForMyLocation = function() {
     });
 };
 
+//-----------------------------------------------------------------------------------------------------
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
 
-
-module.exports = { nextISSTimesForMyLocation };
+module.exports = { nextISSTimesForMyLocation, printPassTimes };
 // module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
 // module.exports = { fetchMyIP, fetchCoordsByIP };
